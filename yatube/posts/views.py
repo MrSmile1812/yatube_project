5780379a0697747df7948161d8e11e4f0266c720
George Prokofev <prokofev987@mail.ru>
2022-12-09 16:14:39 +0300

    Project has been improved

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
 
-# Create your views here.
+
+def index(request):
+    template = 'posts/index.html'
+    title = 'Последние обновления на сайте'
+    posts = Post.objects.order_by('-pub_date')[:10]
+    context = {
+        'posts': posts,
+        'title':title
+    }
+    return render(request, template, context)
+
+def group_posts(request, slug):
+    template = 'posts/group_list.html'
+    group = get_object_or_404(Group, slug=slug)
+    title = f'Записи сообщества "{group}"'
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'group': group,
+        'posts': posts,
+        'title':title
+    }
+    return render(request, template, context)
